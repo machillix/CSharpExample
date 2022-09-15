ca9dca3b8bde99c77a22e5d6fca7f00215b519e5
Toni Westerlund <toni.westerlund@lapinamk.fi>
2022-09-15 12:01:22 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -25,7 +25,7 @@ namespace MyApp
             float b = 4;
             float c = (float)Math.Sqrt(Math.Pow(a,2f)+Math.Pow(b,2f));
 
-            Console.WriteLine("aaHypotenuusan pituus : " + c);
+            Console.WriteLine("Hypotenuusan pituus : " + c);
 
             // Tuntiharjoitus 3.
 
@@ -34,6 +34,42 @@ namespace MyApp
             String[] nameSplit = fullName.Split(' ');
             Console.WriteLine("Etunimesi : " + nameSplit[0]);
             Console.WriteLine("Sukunimesi : " + nameSplit[1]);
+
+            // Tuntiharjoitus 4.
+            Console.WriteLine("aaHypotenuusan pituus : " + Pythagora(3,4));
+
+            // Tuntiharjoitus 5.
+            Console.WriteLine("Vastaus : " + MultiplyTen());
+
+            // Tuntiharjoitus 6.
+            Console.WriteLine("Syötä Etunimesi ja Sukunimesi");
+            fullName = Console.ReadLine();
+            nameSplit = fullName.Split(' ');
+
+            if(nameSplit.Length < 2){
+                Console.WriteLine("Syötä etu ja sukunimesi");
+            } else if (nameSplit.Length > 2){
+                Console.WriteLine("Syötä pelkästään etu ja sukunimesi");
+            }else{
+                Console.WriteLine("Etunimesi : " + nameSplit[0]);
+                Console.WriteLine("Sukunimesi : " + nameSplit[1]);
+            }
+            
+
+
+        }
+
+
+        static float Pythagora(float a, float b){
+
+            return (float)Math.Sqrt(Math.Pow(a,2f)+Math.Pow(b,2f));
+        }
+
+        static int MultiplyTen(){
+
+            Console.WriteLine("Minka luvun haluat kertoa kymmenellä");
+            int number = int.Parse(Console.ReadLine());
+            return number * 10;
         }
     }
 }
