55c97a37bd0028ca0efff34091de8da92fff4697
Toni Westerlund <toni.westerlund@lapinamk.fi>
2022-09-15 09:57:38 +0300

    Add 1-3 Examples

--- Program.cs
+++ Program.cs
@@ -1,2 +1,39 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace MyApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Tuntiharjoitus 1.
+            float numberOne = 4;
+            float numberTwo = 3;
+
+            float div = numberOne / numberTwo;
+            float mul = numberOne * numberTwo;
+            float sub = numberOne - numberTwo;
+            float sum = numberOne + numberTwo;
+
+            Console.WriteLine("Jakolasku : " + div);
+            Console.WriteLine("Kertolasku : " + mul);
+            Console.WriteLine("Vähennyslasku : " + sub);
+            Console.WriteLine("Yhteenlasku : " + sum);
+
+            // Tuntiharjoitus 2.
+            float a = 3;
+            float b = 4;
+            float c = (float)Math.Sqrt(Math.Pow(a,2f)+Math.Pow(b,2f));
+
+            Console.WriteLine("aaHypotenuusan pituus : " + c);
+
+            // Tuntiharjoitus 3.
+
+            Console.WriteLine("Syötä Etunimesi ja Sukunimesi");
+            string fullName = Console.ReadLine();
+            String[] nameSplit = fullName.Split(' ');
+            Console.WriteLine("Etunimesi : " + nameSplit[0]);
+            Console.WriteLine("Sukunimesi : " + nameSplit[1]);
+        }
+    }
+}
